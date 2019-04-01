278965d9553ce8b5579797412c6e29071069e63d
Tatallias <jorge.fulgencioesteves@heig-vd.ch>
2019-04-01 14:36:34 +0200

    Setting up maven project

---
+++ Labo3_SMTP/src/main/java/RES/Labo3_SMTP/App.java
@@ -0,0 +1,13 @@
+package RES.Labo3_SMTP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
