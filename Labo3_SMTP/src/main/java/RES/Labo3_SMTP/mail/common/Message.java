f1712d369ea6913c4938298a257432eb3e02403b
Eric-Noel <eric88n@gmail.com>
2019-04-14 18:38:50 +0200

    more

--- Labo3_SMTP/src/main/java/RES/Labo3_SMTP/mail/common/Message.java
+++ Labo3_SMTP/src/main/java/RES/Labo3_SMTP/mail/common/Message.java
@@ -16,7 +16,7 @@ public class Message {
         this.message = message;
     }
 
-
+    
 
 
 
--- Labo3_SMTP/src/main/java/RES/Labo3_SMTP/smtp/SmtpClient.java
+++ Labo3_SMTP/src/main/java/RES/Labo3_SMTP/smtp/SmtpClient.java
@@ -1,4 +1,56 @@
 package RES.Labo3_SMTP.smtp;
 
+
+import RES.Labo3_SMTP.mail.common.Message;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.logging.Logger;
+
 public class SmtpClient {
+
+
+
+
+    private static final Logger LOG = Logger.getLogger(SmtpClient.class.getName());
+
+    private String serverAddress;
+    private int port;
+    private Socket socket;
+    private PrintWriter writer;
+    private BufferedReader reader;
+
+
+    public SmtpClient(String serverAddress, int port){
+        this.serverAddress = serverAddress;
+        this.port = port;
+    }
+
+
+
+    public void sendMessage(Message message) throws IOException{
+
+        LOG.info("Sending message");
+        socket = new Socket(serverAddress, port);
+        writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"),true);
+        reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+        String readLine = reader.readLine();
+        LOG.info(readLine);
+        writer.write("EHLO localhost \r\n");
+        readLine = reader.readLine();
+        LOG.info(readLine);
+        if(!readLine.startsWith("250")){
+            throw new IOException("error : " + readLine);
+        }
+        while (readLine.startsWith("250-")){
+            readLine = reader.readLine();
+            LOG.info(readLine);
+        }
+
+        //writer.write("MAIL FROM:" + message);
+
+
+    }
+
+
 }
